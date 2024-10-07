1970c1f9cb9e6f6bcdf0d521d8da821462a9466a
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2024-10-07 13:08:44 +0530

    add config class

--- src/main/java/edu/icet/entity/PatientEntity.java
+++ src/main/java/edu/icet/entity/PatientEntity.java
@@ -4,15 +4,17 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "patients")
 public class PatientEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String nic;
--- src/main/java/edu/icet/service/PatientServiceImpl.java
+++ src/main/java/edu/icet/service/PatientServiceImpl.java
@@ -1,22 +1,32 @@
 package edu.icet.service;
 
 import edu.icet.dto.Patient;
+import edu.icet.entity.PatientEntity;
+import edu.icet.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
+import org.modelmapper.ModelMapper;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PatientServiceImpl implements PatientService{
-    List<Patient> patientList=new ArrayList<>();
+   
+
+
     final ModelMapper mapper;
+    final PatientRepository repository;
     @Override
     public void addPatient(Patient patient) {
-        patientList.add(patient);
+        repository.save(mapper.map(patient, PatientEntity.class));
     }
 
     @Override
     public List<Patient> getPatient() {
-        return patientList;
+        List<Patient> patients =new ArrayList<>();
+        repository.findAll().forEach(patient -> patients.add(mapper.map(patient,Patient.class)));
+        return patients;
     }
 }
