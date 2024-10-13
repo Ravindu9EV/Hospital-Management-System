1142766fc592a5e1ed6744c0aa99b0452f2e109e
Yasantha Ravindu <99851400+ravindu9ev@users.noreply.github.com>
2024-10-13 16:40:11 +0530

    add search methods

--- src/main/java/edu/icet/service/PatientService.java
+++ src/main/java/edu/icet/service/PatientService.java
@@ -1,10 +1,25 @@
 package edu.icet.service;
 
 import edu.icet.dto.Patient;
+import edu.icet.entity.PatientEntity;
 
 import java.util.List;
 
 public interface PatientService {
     void addPatient(Patient patient);
     List<Patient> getPatient();
+    void deleteById(Integer id);
+    boolean updateById(Integer id);
+
+    List<Patient> findByName(String name);
+
+    List<Patient> findByBloodGroup(String bloodGroup);
+
+    List<Patient> findByAddress(String address);
+
+    List<Patient> findByNic(String nic);
+
+    List<Patient> findByCategory(String category);
+
+    List<Patient> findByContact(String contact);
 }
